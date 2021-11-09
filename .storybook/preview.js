142ce23bb057c9a138918ff38a17ca5f52a45036
fselvino <fernandoselvino@gmail.com>
2021-11-08 21:31:46 -0300

    base

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+
+
+import { RouterContext } from 'next/dist/shared/lib/router-context'
+import GlobalStyles from '../src/styles/global'
+
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider
+  }
+}
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  )
+]
